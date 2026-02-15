16951952865494b32993f0d56dd5a330f926ead9
20248056 <20248056@aloe.ulima.edu.pe>
2026-02-15 17:17:07 -0500

    Update PartidoPolitico.java

--- src/Modelo/PartidoPolitico.java
+++ src/Modelo/PartidoPolitico.java
@@ -1,3 +1,14 @@
 public class PartidoPolitico {
+  //Atributos
+    private String nombrePartido;
+    private String sigla;
+    private String logo;
+    private String representanteLegal;
 
+    //Consturctor con parametros
+
+    public PartidoPolitico(){
+        
+    }
 }
+
