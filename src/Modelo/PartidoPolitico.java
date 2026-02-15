7d9c5885143dda63825636d64cec648139ae5957
20248056 <20248056@aloe.ulima.edu.pe>
2026-02-15 17:25:47 -0500

    avance getter setter

--- src/Modelo/PartidoPolitico.java
+++ src/Modelo/PartidoPolitico.java
@@ -7,8 +7,45 @@ public class PartidoPolitico {
 
     //Consturctor con parametros
 
-    public PartidoPolitico(){
-        
+    public PartidoPolitico(String nombrePartido,String sigla,String logo,String representanteLegal){
+        this.nombrePartido = nombrePartido;
+        this.sigla = sigla;
+        this.logo = logo;
+        this.representanteLegal = representanteLegal;
     }
+
+    //Getters y setters
+    public String getNombrePartido() {
+        return nombrePartido;
+    }
+
+    public void setNombrePartido(String nombrePartido) {
+        this.nombrePartido = nombrePartido;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public String getRepresentanteLegal() {
+        return representanteLegal;
+    }
+
+    public void setRepresentanteLegal(String representanteLegal) {
+        this.representanteLegal = representanteLegal;
+    }
+
 }
 
